280cbd092b6b39e96f105d23303356d8926f82ba
APTX-4869 <hl_jiangtao@163.com>
2026-06-23 22:51:45 +0800

    增加apollo配置

--- backend/src/main/java/cn/gt/smsbilling/SmsBillingApplication.java
+++ backend/src/main/java/cn/gt/smsbilling/SmsBillingApplication.java
@@ -1,5 +1,6 @@
 package cn.gt.smsbilling;
 
+import com.ctrip.framework.apollo.bootstrap.ApolloApplicationListener;
 import com.github.lianjiatech.retrofit.spring.boot.core.RetrofitScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @RetrofitScan("cn.gt.smsbilling.gateway")
 public class SmsBillingApplication {
     public static void main(String[] args) {
+        // 启用 Apollo 配置刷新
+        ApolloApplicationListener.refreshDynamicConfig();
         SpringApplication.run(SmsBillingApplication.class, args);
     }
 }
